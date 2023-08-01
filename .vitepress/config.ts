8c5abd1f67c8ca98265afd67cb03078ada01a17d
Taylor Otwell <taylor@laravel.com>
2023-08-01 15:06:13 -0500

    wip

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -6,7 +6,7 @@ export default defineConfigWithTheme<ThemeConfig>({
     extends: config,
     title: 'Envoyer',
     description: 'Zero Downtime PHP Deployments',
-    base: '/docs/',
+    base: '/',
     cleanUrls: true,
     srcDir: 'src',
 
